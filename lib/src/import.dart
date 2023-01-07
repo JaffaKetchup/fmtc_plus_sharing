bbacf2c874827e6aa0b5d848f1ea8b860f94c9cb
JaffaKetchup <github@jaffaketchup.dev>
2023-01-07 22:26:33 +0000

    Improved import system collision handling Reduced migration times from pre-v7 versions Fixed bugs and improved stability

--- lib/src/import.dart
+++ lib/src/import.dart
@@ -12,41 +12,6 @@ import 'package:flutter_map_tile_caching/fmtc_module_api.dart';
 import 'package:isar/isar.dart';
 import 'package:path/path.dart' as p;
 
-/// Describes the result of a store's import
-///
-/// Property of [ImportResult].
-enum ImportResultCategory {
-  /// The store was imported successfully
-  successful,
-
-  /// There was an existing store with the same name and `overwriteExistingStore`
-  /// was `false`
-  collision,
-
-  /// There was an unknown error during importing
-  ///
-  /// The database system should be in a stable state.
-  unknown,
-}
-
-/// Describes a store import entity after success/failure
-class ImportResult {
-  ImportResult._({
-    required this.path,
-    required this.storeName,
-    required this.result,
-  });
-
-  /// The path to the selected file
-  final String path;
-
-  /// The store name of the selected file (parsed from filename)
-  final String storeName;
-
-  /// Describes the status of the import
-  final Future<ImportResultCategory> result;
-}
-
 /// Extends [RootImport] (accessed through [RootDirectory.import]) with the
 /// import functionality
 extension FMTCImportSharingModule on RootImport {
@@ -57,20 +22,22 @@ extension FMTCImportSharingModule on RootImport {
   /// can be picked, but only those with the [fileExtension] extension will be
   /// processed.
   ///
-  /// Enabling [overwriteExistingStore] (defaults to `false`) will cause the
-  /// import to overwrite any store of the same name.
-  ///
   /// Disabling [emptyCacheBeforePicking] is not recommended (defaults to
   /// `true`). When disabled, the picker may use cached files as opposed to the
   /// real files, which may yield unexpected results. This is only effective on
   /// Android and iOS - other platforms cannot use caching.
   ///
-  /// If any files are selected, a map of the selected filenames to whether that
-  /// store was imported successfully is returned.
-  Future<Iterable<ImportResult>?> withGUI({
+  /// Setting [collisionHandler] allows for custom behaviour in the event that
+  /// a store with the same name already exists. If it returns `true`, the store
+  /// will be overwritten, otherwise the import will fail.
+  ///
+  /// Returns a [Map] of the store names to the result status of the respective
+  /// import (`true` for successful, `false` for failed), if any valid files are
+  /// selected.
+  Future<Map<String, Future<bool>>?> withGUI({
     String fileExtension = 'fmtc',
-    bool overwriteExistingStore = false,
     bool emptyCacheBeforePicking = true,
+    Future<bool> Function(String storeName)? collisionHandler,
   }) async {
     if (emptyCacheBeforePicking && (Platform.isAndroid || Platform.isIOS)) {
       await FilePicker.platform.clearTemporaryFiles();
@@ -91,42 +58,53 @@ extension FMTCImportSharingModule on RootImport {
       );
     }
 
-    if (importPaths == null) return null;
-
-    return importPaths.files.where((f) => f.extension == fileExtension).map(
+    final files = importPaths?.files.where((f) => f.extension == fileExtension);
+    if (files == null || files.isEmpty) return null;
+    return files
+        .map(
           (p) => manual(
             File(p.path!),
-            overwriteExistingStore: overwriteExistingStore,
+            collisionHandler: collisionHandler,
           ),
-        );
+        )
+        .reduce((a, b) => a..addAll(b));
   }
 
   /// Import a store from a specified [inputFile]
   ///
+  /// Setting [collisionHandler] allows for custom behaviour in the event that
+  /// a store with the same name already exists. If it returns `true`, the store
+  /// will be overwritten, otherwise the import will fail.
+  ///
   /// Also see [withGUI] for a prebuilt solution to allow the user to select
   /// files to import.
   ///
-  /// Enabling [overwriteExistingStore] (defaults to `false`) will cause the
-  /// import to overwrite any store of the same name.
-  ImportResult manual(
+  /// Returns a [Map] of the store name to the result status of the import
+  /// (`true` for successful, `false` for failed).
+  Map<String, Future<bool>> manual(
     File inputFile, {
-    bool overwriteExistingStore = false,
+    Future<bool> Function(String storeName)? collisionHandler,
   }) {
-    final filename = p.basenameWithoutExtension(inputFile.path);
-    final storeName =
-        filename.substring(filename.startsWith('export_') ? 7 : 0);
+    final storeName = p.basenameWithoutExtension(inputFile.path).substring(
+          p.basenameWithoutExtension(inputFile.path).startsWith('export_')
+              ? 7
+              : 0,
+        );
 
-    return ImportResult._(
-      path: inputFile.absolute.path,
-      storeName: storeName,
-      result: (() async {
+    return {
+      storeName: () async {
+        if (!await inputFile.exists()) return false;
+
         if (FMTC.instance(storeName).manage.ready) {
-          if (!overwriteExistingStore) return ImportResultCategory.collision;
+          if (collisionHandler == null || !await collisionHandler(storeName)) {
+            return false;
+          }
           await FMTC.instance(storeName).manage.delete();
         }
 
         final id = DatabaseTools.hash(storeName);
         final newStorePath = FMTC.instance.rootDirectory.directory > '$id.isar';
+
         try {
           await inputFile.copy(newStorePath);
           FMTCRegistry.instance.storeDatabases[id] = await Isar.open(
@@ -139,11 +117,11 @@ extension FMTCImportSharingModule on RootImport {
         } catch (_) {
           await File(newStorePath).delete();
           FMTCRegistry.instance.storeDatabases.remove(id);
-          return ImportResultCategory.unknown;
+          return false;
         }
 
-        return ImportResultCategory.successful;
-      })(),
-    );
+        return true;
+      }(),
+    };
   }
 }
