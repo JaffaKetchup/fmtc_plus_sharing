fe92a3428a5d4731afd6e0166c05c2d1890f47d3
Luka S <github@jaffaketchup.dev>
2023-05-05 21:11:31 +0100

    v8: Support FMTC v8 (#1)
    * Reworked importing implementation
    
    * Updated CHANGELOG and version
    Minor improvements
    
    * Upgrade to Isar v3.1.0+1
    
    * Prepare for v8 release

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
-## 7.0.0 - 2023/01/XX
+## [8.0.0] - 2023/XX/XX
+
+* Improved stability and removed bugs from store importing
+* Added support for 'flutter_map_tile_caching' v8
+
+## [7.0.0] - 2023/01/XX
 
 * Initial release
--- lib/src/import.dart
+++ lib/src/import.dart
@@ -3,6 +3,7 @@
 
 // ignore_for_file: invalid_use_of_internal_member, invalid_use_of_protected_member
 
+import 'dart:async';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
@@ -15,7 +16,7 @@ import 'package:path/path.dart' as p;
 /// Extends [RootImport] (accessed through [RootDirectory.import]) with the
 /// import functionality
 extension FMTCImportSharingModule on RootImport {
-  /// Import store(s) with the platform specifc file picker interface
+  /// Import store files with the platform specifc file picker interface
   ///
   /// Where supported, the user will only be able to pick files with the
   /// [fileExtension] extension ('fmtc' by default). If not supported, any file
@@ -29,15 +30,14 @@ extension FMTCImportSharingModule on RootImport {
   ///
   /// Setting [collisionHandler] allows for custom behaviour in the event that
   /// a store with the same name already exists. If it returns `true`, the store
-  /// will be overwritten, otherwise the import will fail.
+  /// will be overwritten, otherwise (and by default) the import will fail.
   ///
-  /// Returns a [Map] of the store names to the result status of the respective
-  /// import (`true` for successful, `false` for failed), if any valid files are
-  /// selected.
-  Future<Map<String, Future<bool>>?> withGUI({
+  /// Returns a [Map] of the input filename to its corresponding [ImportResult].
+  Future<Map<String, Future<ImportResult>>?> withGUI({
     String fileExtension = 'fmtc',
     bool emptyCacheBeforePicking = true,
-    Future<bool> Function(String storeName)? collisionHandler,
+    FutureOr<bool> Function(String filename, String storeName)?
+        collisionHandler,
   }) async {
     if (emptyCacheBeforePicking && (Platform.isAndroid || Platform.isIOS)) {
       await FilePicker.platform.clearTemporaryFiles();
@@ -60,68 +60,150 @@ extension FMTCImportSharingModule on RootImport {
 
     final files = importPaths?.files.where((f) => f.extension == fileExtension);
     if (files == null || files.isEmpty) return null;
-    return files
-        .map(
-          (p) => manual(
-            File(p.path!),
-            collisionHandler: collisionHandler,
-          ),
-        )
-        .reduce((a, b) => a..addAll(b));
+    return manual(
+      files.map((e) => File(e.path!)).toList(),
+      collisionHandler: collisionHandler,
+    );
   }
 
-  /// Import a store from a specified [inputFile]
+  /// Import stores from specified [inputFiles]
   ///
   /// Setting [collisionHandler] allows for custom behaviour in the event that
   /// a store with the same name already exists. If it returns `true`, the store
-  /// will be overwritten, otherwise the import will fail.
+  /// will be overwritten, otherwise (and by default) the import will fail.
+  ///
+  /// Note that there are no checks to confirm that the listed [inputFiles] are
+  /// valid Isar databases. Attempting to import other files or corrupted
+  /// databases may cause the app to crash.
   ///
   /// Also see [withGUI] for a prebuilt solution to allow the user to select
   /// files to import.
   ///
-  /// Returns a [Map] of the store name to the result status of the import
-  /// (`true` for successful, `false` for failed).
-  Map<String, Future<bool>> manual(
-    File inputFile, {
-    Future<bool> Function(String storeName)? collisionHandler,
-  }) {
-    final storeName = p.basenameWithoutExtension(inputFile.path).substring(
-          p.basenameWithoutExtension(inputFile.path).startsWith('export_')
-              ? 7
-              : 0,
-        );
+  /// Returns a [Map] of the input filename to its corresponding [ImportResult].
+  Map<String, Future<ImportResult>> manual(
+    List<File> inputFiles, {
+    FutureOr<bool> Function(String filename, String storeName)?
+        collisionHandler,
+  }) =>
+      Map.fromEntries(
+        inputFiles.map(
+          (f) => MapEntry(
+            p.basename(f.path),
+            () async {
+              // Quit if the input file no longer exists
+              if (!await f.exists()) {
+                return const ImportResult._(storeName: null, successful: false);
+              }
 
-    return {
-      storeName: () async {
-        if (!await inputFile.exists()) return false;
+              // Create the temporary directory
+              final tmpDir = FMTC.instance.rootDirectory.directory >> 'import';
+              await tmpDir.create();
 
-        if (FMTC.instance(storeName).manage.ready) {
-          if (collisionHandler == null || !await collisionHandler(storeName)) {
-            return false;
-          }
-          await FMTC.instance(storeName).manage.delete();
-        }
+              // Construct temporary structures to read the Isar database at an
+              // appropriate location
+              final tmpPath = tmpDir >
+                  '.import${DateTime.now().millisecondsSinceEpoch}.isar';
+              final tmpFile = File(tmpPath);
+              Isar? tmpDb;
 
-        final id = DatabaseTools.hash(storeName);
-        final newStorePath = FMTC.instance.rootDirectory.directory > '$id.isar';
+              // Copy the target file to the temporary file and try to open it
+              await f.copy(tmpPath);
+              try {
+                tmpDb = await Isar.open(
+                  [DbStoreDescriptorSchema, DbTileSchema, DbMetadataSchema],
+                  name: tmpPath.replaceAll('.isar', ''),
+                  directory: tmpDir.absolute.path,
+                  maxSizeMiB: FMTC.instance.settings.databaseMaxSize,
+                  compactOnLaunch:
+                      FMTC.instance.settings.databaseCompactCondition,
+                  inspector: FMTC.instance.debugMode,
+                );
+              } catch (_) {
+                if (tmpDb != null && tmpDb.isOpen) await tmpDb.close();
+                if (await tmpDir.exists()) await tmpDir.delete(recursive: true);
+                return const ImportResult._(storeName: null, successful: false);
+              }
 
-        try {
-          await inputFile.copy(newStorePath);
-          FMTCRegistry.instance.storeDatabases[id] = await Isar.open(
-            [DbStoreDescriptorSchema, DbTileSchema, DbMetadataSchema],
-            name: id.toString(),
-            directory: FMTC.instance.rootDirectory.directory.path,
-            maxSizeMiB: FMTC.instance.settings.databaseMaxSize,
-            compactOnLaunch: FMTC.instance.settings.databaseCompactCondition,
-          );
-        } catch (_) {
-          await File(newStorePath).delete();
-          FMTCRegistry.instance.storeDatabases.remove(id);
-          return false;
-        }
+              // Read the store name from within the temporary database, closing
+              // it afterward
+              final storeName = (await tmpDb.storeDescriptor.get(0))?.name;
+              if (storeName == null) {
+                return const ImportResult._(storeName: null, successful: false);
+              }
+              if (tmpDb.isOpen) await tmpDb.close();
 
-        return true;
-      }(),
-    };
-  }
+              // Check if there is a conflict with an existing store
+              if (FMTC.instance(storeName).manage.ready) {
+                if (!await (collisionHandler?.call(
+                      p.basename(f.path),
+                      storeName,
+                    ) ??
+                    false)) {
+                  if (await tmpDir.exists()) {
+                    await tmpDir.delete(recursive: true);
+                  }
+                  return ImportResult._(
+                    storeName: storeName,
+                    successful: false,
+                  );
+                }
+                await FMTC.instance(storeName).manage.delete();
+              }
+
+              // Calculate the store's ID, and rename the temporary file to it
+              final storeId = DatabaseTools.hash(storeName);
+              await tmpFile.rename(
+                (FMTC.instance.rootDirectory.directory >> '$storeId.isar')
+                    .absolute
+                    .path,
+              );
+
+              // Register the new store instance
+              // Doesn't require error catching, as the same database has already
+              // been opened.
+              FMTCRegistry.instance.register(
+                storeId,
+                await Isar.open(
+                  [DbStoreDescriptorSchema, DbTileSchema, DbMetadataSchema],
+                  name: storeId.toString(),
+                  directory: FMTC.instance.rootDirectory.directory.path,
+                  maxSizeMiB: FMTC.instance.settings.databaseMaxSize,
+                  compactOnLaunch:
+                      FMTC.instance.settings.databaseCompactCondition,
+                  inspector: FMTC.instance.debugMode,
+                ),
+              );
+
+              // Delete temporary structures
+              if (await tmpDir.exists()) await tmpDir.delete(recursive: true);
+
+              return ImportResult._(storeName: storeName, successful: true);
+            }(),
+          ),
+        ),
+      );
+}
+
+/// Represents the state of an import
+///
+/// Note that [storeName] may be different to the import file's name. In this
+/// case, the [MapEntry] for which this object is the value will have the
+/// filename as the key.
+class ImportResult {
+  /// The store name, as it will be imported
+  ///
+  /// Note that this may be different to the import file's name. In this case,
+  /// the [MapEntry] for which this object is the value will have the filename
+  /// as the key.
+  final String? storeName;
+
+  /// Whether or not this import was successfully completed
+  ///
+  /// Will be false if the collision handler prevents this store's import.
+  final bool successful;
+
+  const ImportResult._({
+    required this.storeName,
+    required this.successful,
+  });
 }
